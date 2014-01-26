e2377775dcf62ddf53d6ab3703a931534b5ebea4
Beni Arnold <banjipark@gmail.com>
2014-01-26 23:08:29 +0100

    Now a commit with the actual changes. -.-

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
 import SimpleJSON
+import PutJSON
 
-main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
+main = print (renderJValue (JObject [("foo", JNumber 1), ("bar", JBool False)]))
